c1f0083d7f2ea72f4e48ee7676363bb393f72d18
sergey <sergey.pavlov@uniter1.tech>
2022-03-01 22:09:16 +0300

    devel-packages: пытался писать мокнутый тест

---
+++ src/Infrastructure/Request/GenerateClient.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PhpUniter\PackageLaravel\Infrastructure\Request;
+
+use GuzzleHttp\Client;
+
+class GenerateClient extends Client
+{
+
+}
--- src/PhpUniterPackageLaravelServiceProvider.php
+++ src/PhpUniterPackageLaravelServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 use PhpUniter\PackageLaravel\Controller\Console\Cli\GeneratePhpUniterTestCommand;
 use PhpUniter\PackageLaravel\Infrastructure\Integrations\PhpUniterIntegration;
+use PhpUniter\PackageLaravel\Infrastructure\Request\GenerateClient;
 use PhpUniter\PackageLaravel\Infrastructure\Request\GenerateRequest;
 
 class PhpUniterPackageLaravelServiceProvider extends ServiceProvider
@@ -43,22 +44,26 @@ class PhpUniterPackageLaravelServiceProvider extends ServiceProvider
 
         $this->app->bind(PhpUniterIntegration::class, function (Application $app) {
             return new PhpUniterIntegration(
-                new Client(),
+                new GenerateClient(),
                 $app->make(GenerateRequest::class)
             );
         });
 
         $this->app->bind(GenerateRequest::class, function (Application $app) {
             return new GenerateRequest(
-            'POST',
-                config('php-uniter.baseUrl').'/api/v1/generator/generate',
+                'POST',
+                config('php-uniter.baseUrl') . '/api/v1/generator/generate',
                 [
                     'auth' => [
-                        'Authorization' => 'Bearer '.config('php-uniter.accessToken'),
+                        'Authorization' => 'Bearer ' . config('php-uniter.accessToken'),
                     ],
                     'timeout' => 2,
                 ]
             );
+        });
+
+        $this->app->bind(GenerateClient::class, function (Application $app) {
+            return new GenerateClient();
         });
     }
 }
