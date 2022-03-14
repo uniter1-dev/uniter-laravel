ce8367fb26c4d6d206d7f41d716330d99bdc3ad7
sergey <sergey.pavlov@uniter1.tech>
2022-03-14 21:06:14 +0300

    devel-packages: перенес обфускацию в Obfuscator

--- src/Application/Obfuscator/Entity/ObfuscatedClass.php
+++ src/Application/Obfuscator/Entity/ObfuscatedClass.php
@@ -4,8 +4,9 @@ namespace PhpUniter\PackageLaravel\Application\Obfuscator\Entity;
 
 use Closure;
 use PhpUniter\PackageLaravel\Application\File\Entity\LocalFile;
-use PhpUniter\PackageLaravel\Application\Obfuscator\Exception\ObfuscationFailed;
+
 use PhpUniter\PackageLaravel\Application\Obfuscator\Obfuscated;
+use PhpUniter\PackageLaravel\Application\Obfuscator\Obfuscator;
 
 class ObfuscatedClass implements Obfuscated
 {
@@ -22,87 +23,23 @@ class ObfuscatedClass implements Obfuscated
 
     public function getObfuscatedFileBody(): string
     {
-        return $this->getObfuscated($this->map, $this->localFile);
-    }
-
-    public function getObfuscated(ObfuscateMap $map, LocalFile $localFile): string
-    {
-        $callback = Closure::fromCallable(function ($matches) use ($map) {
-            return $this->map->storeKeysAs($map::CLASS_NAMES, $matches, $this->getUniqueKey());
-        });
-
-        $obfuscated = preg_replace_callback_array(
-            $replacements = [
-                '/(?<=class\s)(\w+)/'    => $callback,
-                '/(?<=function\s)(\w+)/' => function ($matches) use ($map) {
-                    return $this->map->storeKeysAs($map::METHODS, $matches, $this->getUniqueKey());
-                },
-                '/(?<=const\s)(\w+)/' => function ($matches) use ($map) {
-                    return $this->map->storeKeysAs($map::CONSTANTS, $matches, $this->getUniqueKey());
-                },
-                '/(?<=namespace\s)(.+)/' => function ($matches) use ($map) {
-                    return $this->map->storeKeysAs($map::NAMESPACES, $matches, $this->getUniqueKey().';');
-                },
-            ],
-            $localFile->getFileBody(),
-            -1,
-            $count
-        );
-
-        if (count($replacements) > $count) {
-            throw new ObfuscationFailed("Obfuscation failed on {$localFile->getFilePath()}, count of replacements is not enough");
-        }
-
-        foreach ($map->getMap()[$map::METHODS] as $pair) {
-            $obfuscated = self::replaceInText('->', $pair, $obfuscated, '(');
-            $obfuscated = self::replaceInText('::', $pair, $obfuscated, '(');
-        }
-
-        foreach ($map->getMap()[$map::CONSTANTS] as $pair) {
-            $obfuscated = self::replaceInText('::', $pair, $obfuscated);
-        }
-
-        return $obfuscated;
+        return Obfuscator::getObfuscated($this->map, $this->localFile, [$this, 'getKeySaver']);
     }
 
     public function deObfuscate(string $fileBody): string
     {
-        return self::deObf($this->map, $fileBody);
+        return Obfuscator::deObf($this->map, $fileBody);
     }
 
-    public static function deObf(ObfuscateMap $map, string $fileBody): string
+    public function getKeySaver(ObfuscateMap $map, string $mapKey): callable
     {
-        $deObfuscated = $fileBody;
-
-        foreach ($map->getMap()[$map::CLASS_NAMES] as $methodPair) {
-            $deObfuscated = str_replace($methodPair[0], $methodPair[1], $deObfuscated);
-        }
-
-        foreach ($map->getMap()[$map::METHODS] as $methodPair) {
-            $deObfuscated = str_replace($methodPair[0], $methodPair[1], $deObfuscated);
-        }
-        foreach ($map->getMap()[$map::CONSTANTS] as $methodPair) {
-            $deObfuscated = str_replace($methodPair[0], $methodPair[1], $deObfuscated);
-        }
-        foreach ($map->getMap()[$map::NAMESPACES] as $methodPair) {
-            $deObfuscated = str_replace($methodPair[0], $methodPair[1], $deObfuscated);
-        }
-
-        return $deObfuscated;
+        return function ($matches) use ($map, $mapKey) {
+            return $map->storeKeysAs($mapKey, $matches, $this->getUniqueKey());
+        };
     }
 
-    private static function replaceInText($prefix, $pair, $subject, $suffix = '')
-    {
-        $methodInText = $prefix.$pair[1].$suffix;
-
-        return str_replace($methodInText, $prefix.$pair[0].$suffix, $subject);
-    }
-
-    private function getUniqueKey(): string
+    public function getUniqueKey(): string
     {
         return ($this->keyGenerator)();
     }
-
-
-
 }
--- src/Application/Obfuscator/Obfuscator.php
+++ src/Application/Obfuscator/Obfuscator.php
@@ -3,6 +3,8 @@
 namespace PhpUniter\PackageLaravel\Application\Obfuscator;
 
 use PhpUniter\PackageLaravel\Application\File\Entity\LocalFile;
+use PhpUniter\PackageLaravel\Application\Obfuscator\Entity\ObfuscateMap;
+use PhpUniter\PackageLaravel\Application\Obfuscator\Exception\ObfuscationFailed;
 use PhpUniter\PackageLaravel\Application\PhpUniter\Entity\PhpUnitTest;
 
 class Obfuscator
@@ -20,4 +22,66 @@ class Obfuscator
 
         return new PhpUnitTest($obfuscatedPhpUnitTest->getLocalFile(), $res, []);
     }
+
+
+    public static function getObfuscated(ObfuscateMap $map, LocalFile $localFile, callable $getKeySaver): string
+    {
+        $obfuscated = preg_replace_callback_array(
+            $replacements = [
+                '/(?<=class\s)(\w+)/'    => $getKeySaver($map, $map::CLASS_NAMES),
+                '/(?<=function\s)(\w+)/' => $getKeySaver($map, $map::METHODS),
+                '/(?<=const\s)(\w+)/'    => $getKeySaver($map, $map::CONSTANTS),
+                '/(?<=namespace\s)(.+)/' => $getKeySaver($map, $map::NAMESPACES),
+            ],
+            $localFile->getFileBody(),
+            -1,
+            $count
+        );
+
+        if (count($replacements) > $count) {
+            throw new ObfuscationFailed("Obfuscation failed on {$localFile->getFilePath()}, count of replacements is not enough");
+        }
+
+        foreach ($map->getMap()[$map::METHODS] as $pair) {
+            $obfuscated = self::replaceInText('->', $pair, $obfuscated, '(');
+            $obfuscated = self::replaceInText('::', $pair, $obfuscated, '(');
+        }
+
+        foreach ($map->getMap()[$map::CONSTANTS] as $pair) {
+            $obfuscated = self::replaceInText('::', $pair, $obfuscated);
+        }
+
+        return $obfuscated;
+    }
+
+
+
+    public static function deObf(ObfuscateMap $map, string $fileBody): string
+    {
+        $deObfuscated = $fileBody;
+
+        foreach ($map->getMap()[$map::CLASS_NAMES] as $methodPair) {
+            $deObfuscated = str_replace($methodPair[0], $methodPair[1], $deObfuscated);
+        }
+
+        foreach ($map->getMap()[$map::METHODS] as $methodPair) {
+            $deObfuscated = str_replace($methodPair[0], $methodPair[1], $deObfuscated);
+        }
+        foreach ($map->getMap()[$map::CONSTANTS] as $methodPair) {
+            $deObfuscated = str_replace($methodPair[0], $methodPair[1], $deObfuscated);
+        }
+        foreach ($map->getMap()[$map::NAMESPACES] as $methodPair) {
+            $deObfuscated = str_replace($methodPair[0], $methodPair[1], $deObfuscated);
+        }
+
+        return $deObfuscated;
+    }
+
+    private static function replaceInText($prefix, $pair, $subject, $suffix = '')
+    {
+        $methodInText = $prefix.$pair[1].$suffix;
+
+        return str_replace($methodInText, $prefix.$pair[0].$suffix, $subject);
+    }
+
 }
