d643cb3a3fe0c0fd0cd33afcc373bfdf8ee545d5
sergey <sergey.pavlov@uniter1.tech>
2022-03-14 21:38:21 +0300

    devel-packages: ObfuscatedClass implements Obfuscated

--- src/Application/Obfuscator/Obfuscated.php
+++ src/Application/Obfuscator/Obfuscated.php
@@ -2,6 +2,13 @@
 
 namespace PhpUniter\PackageLaravel\Application\Obfuscator;
 
+use Closure;
+use PhpUniter\PackageLaravel\Application\File\Entity\LocalFile;
+
 interface Obfuscated
 {
+    public function __construct(LocalFile $localFile, Closure $uniqKeyGenerator, Obfuscator $obfuscator);
+    public function getObfuscatedFileBody(): string;
+    public function deObfuscate(string $fileBody): string;
+
 }
