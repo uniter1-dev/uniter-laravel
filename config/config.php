55ab0a4a9b20aa30172ecb22d500d71c167bb013
sergey <sergey.pavlov@uniter1.tech>
2022-04-29 09:11:02 +0300

    version-one: unit test default

--- config/config.php
+++ config/config.php
@@ -9,5 +9,5 @@ return [
     'preprocess'          => env('PHP_UNITER_PREPROCESS', true),
     'obfuscate'           => env('PHP_UNITER_OBFUSCATE', true),
     'unitTestBaseClass'   => env('PHP_UNITER_UNIT_TEST_BASE_CLASS', 'PHPUnit\Framework\TestCase'),
-    'unitTestsDirectory'  => env('PHP_UNITER_UNIT_TESTS_DIRECTORY', 'storage/tests/Unit'),
+    'unitTestsDirectory'  => env('PHP_UNITER_UNIT_TESTS_DIRECTORY', 'tests/Unit'),
 ];
