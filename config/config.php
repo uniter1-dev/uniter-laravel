7163708540c9b052d54b0078ee7a496b10550376
Abax <service@uniter1.tech>
2022-11-29 15:44:29 +0300

    fix: renew config

--- config/config.php
+++ config/config.php
@@ -4,9 +4,16 @@
  * You can place your custom package configuration in here.
  */
 return [
-    'accessToken'        => env('PHP_UNITER_ACCESS_TOKEN'),
-    'baseUrl'            => env('PHP_UNITER_BASE_URL', 'https://uniter1.tech'),
-    'obfuscate'          => env('PHP_UNITER_OBFUSCATE', true),
-    'unitTestBaseClass'  => env('PHP_UNITER_UNIT_TEST_BASE_CLASS', 'PHPUnit\Framework\TestCase'),
-    'unitTestsDirectory' => env('PHP_UNITER_UNIT_TESTS_DIRECTORY', 'tests/Unit'),
+    'accessToken'         => env('PHP_UNITER_ACCESS_TOKEN'),
+    'baseNamespace'       => env('PHP_UNITER_BASE_NAMESPACE', 'Tests\Unit'),
+    'basePath'            => env('PHP_UNITER_BASE_PATH', base_path()),
+    'baseUrl'             => env('PHP_UNITER_BASE_URL', 'https://uniter1.tech'),
+    'helperClass'         => env('PHP_UNITER_HELPER_CLASS', 'PhpUniter\Requester\PhpUnitTestHelper'),
+    'generationPath'      => env('PHP_UNITER_GENERATION_PATH', '/api/v1/generator/generate'),
+    'obfuscate'           => env('PHP_UNITER_OBFUSCATE', true),
+    'preprocess'          => env('PHP_UNITER_PREPROCESS', true),
+    'projectDirectory'    => env('PHP_UNITER_PROJECT_DIRECTORY', base_path()),
+    'registrationPath'    => env('PHP_UNITER_REGISTRATION_PATH', '/api/v1/registration/access-token'),
+    'unitTestBaseClass'   => env('PHP_UNITER_UNIT_TEST_BASE_CLASS', 'PHPUnit\Framework\TestCase'),
+    'unitTestsDirectory'  => env('PHP_UNITER_UNIT_TESTS_DIRECTORY', 'storage/tests/Unit'),
 ];
